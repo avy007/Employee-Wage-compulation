6d9276c2a19046b7c07d14d80d82dad315862afb
akash <akashyewale007@gmail.com>
2023-03-04 06:31:27 +0530

    check emp daily wage

--- src/com/briglab/EmpWage.java
+++ src/com/briglab/EmpWage.java
@@ -1,10 +1,28 @@
 package com.briglab;
 
+import java.util.Random;
+
 public class EmpWage {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Program");
-
+		
+		
+		int emppresent=1;
+		int empabsent=0;
+		int wageperhr=20;
+		int perdayhr=8;
+		int check;
+		
+		Random obj=new Random();
+		check=obj.nextInt(2);
+		
+		if (emppresent==check) {
+			System.out.println("emp present");
+			System.out.println("emp daily wage "+perdayhr*wageperhr );
+		}else {
+			System.out.println("emp absent " + "daily wage=0 ");
+		}
 	}
 
 }
